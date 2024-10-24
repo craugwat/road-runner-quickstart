1c4a51884197a7232869656efd8a6e5671032126
craugwat <craugwat@gmail.com>
2024-10-23 21:31:44 -0500

    removed comments to put back in the rest of the paths for the get yellows

--- TrajectoryActions/src/main/java/com/example/trajectoryactions/Samples/AutoYellowSamples.java
+++ TrajectoryActions/src/main/java/com/example/trajectoryactions/Samples/AutoYellowSamples.java
@@ -34,51 +34,51 @@ public class AutoYellowSamples extends commonTrajectories {
                 .setReversed(true)
                 .splineToLinearHeading(transform(basketPos), xformHeading(Math.PI * 5/4)).build();
 
-//        Action toSecondYellow = drive.actionBuilder(drive.findEndPos(firstYellow2Basket))
-//                .splineToSplineHeading(transform(-53 ,-26, Math.PI), xformHeading(Math.PI)).build();
-//
-//        Action secondYellow2Basket = drive.actionBuilder(drive.findEndPos(toSecondYellow))
-//                .setReversed(true)
-//                .splineToLinearHeading(transform(basketPos), xformHeading(Math.PI * 5/4)).build();
-//
-//        Action toThirdYellow = drive.actionBuilder(drive.findEndPos(secondYellow2Basket))
-//                .splineToSplineHeading(transform(-63 ,-26, Math.PI), xformHeading(Math.PI)).build();
-//
-//        Action thirdYellow2Basket = drive.actionBuilder(drive.findEndPos(toThirdYellow))
-//                .setReversed(true)
-//                .splineToLinearHeading(transform(basketPos), xformHeading(Math.PI * 5/4)).build();
-//
-//        Action toFourthYellow = drive.actionBuilder(drive.findEndPos(thirdYellow2Basket))
-//                .splineToSplineHeading(transform(-24 ,-12, 0), xformHeading(0)).build();
-//
-//        Action fourthYellow2Basket = drive.actionBuilder(drive.findEndPos(toFourthYellow))
-//                .setReversed(true)
-//                .splineToLinearHeading(transform(basketPos), xformHeading(Math.PI * 5/4)).build();
-//
-//        Action park = drive.actionBuilder(drive.findEndPos(fourthYellow2Basket))
-//                .splineToSplineHeading(transform(-24 ,-12, 0), xformHeading(0)).build();
+        Action toSecondYellow = drive.actionBuilder(drive.findEndPos(firstYellow2Basket))
+                .splineToSplineHeading(transform(-53 ,-26, Math.PI), xformHeading(Math.PI)).build();
+
+        Action secondYellow2Basket = drive.actionBuilder(drive.findEndPos(toSecondYellow))
+                .setReversed(true)
+                .splineToLinearHeading(transform(basketPos), xformHeading(Math.PI * 5/4)).build();
+
+        Action toThirdYellow = drive.actionBuilder(drive.findEndPos(secondYellow2Basket))
+                .splineToSplineHeading(transform(-63 ,-26, Math.PI), xformHeading(Math.PI)).build();
+
+        Action thirdYellow2Basket = drive.actionBuilder(drive.findEndPos(toThirdYellow))
+                .setReversed(true)
+                .splineToLinearHeading(transform(basketPos), xformHeading(Math.PI * 5/4)).build();
+
+        Action toFourthYellow = drive.actionBuilder(drive.findEndPos(thirdYellow2Basket))
+                .splineToSplineHeading(transform(-24 ,-12, 0), xformHeading(0)).build();
+
+        Action fourthYellow2Basket = drive.actionBuilder(drive.findEndPos(toFourthYellow))
+                .setReversed(true)
+                .splineToLinearHeading(transform(basketPos), xformHeading(Math.PI * 5/4)).build();
+
+        Action park = drive.actionBuilder(drive.findEndPos(fourthYellow2Basket))
+                .splineToSplineHeading(transform(-24 ,-12, 0), xformHeading(0)).build();
 
         return new SequentialAction(
                 toFirstYellow,
                 actionParameters.collectSample,
-//                new ParallelAction(
-                        firstYellow2Basket
-//                        actionParameters.liftUp
-//                ),
-//                actionParameters.deliverSample,
-//                toSecondYellow,
-//                actionParameters.collectSample,
-//                secondYellow2Basket,
-//                actionParameters.deliverSample,
-//                toThirdYellow,
-//                actionParameters.collectSample,
-//                thirdYellow2Basket,
-//                actionParameters.deliverSample,
-//                toFourthYellow,
-//                actionParameters.collectSample,
-//                fourthYellow2Basket,
-//                actionParameters.deliverSample,
-//                park
+                new ParallelAction(
+                        firstYellow2Basket,
+                        actionParameters.liftUp
+                ),
+                actionParameters.deliverSample,
+                toSecondYellow,
+                actionParameters.collectSample,
+                secondYellow2Basket,
+                actionParameters.deliverSample,
+                toThirdYellow,
+                actionParameters.collectSample,
+                thirdYellow2Basket,
+                actionParameters.deliverSample,
+                toFourthYellow,
+                actionParameters.collectSample,
+                fourthYellow2Basket,
+                actionParameters.deliverSample,
+                park
         );
     }
 }
